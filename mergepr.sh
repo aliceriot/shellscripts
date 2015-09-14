f4143d9462f44b6d6c95d858d64d06728548a5fe
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-14 11:09:19 -0400

    merge and delete a feature branch, push to master

---
+++ mergepr.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -d .git]; then
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    echo "merging "$BRANCH" into master"
+    git checkout master
+    git pull origin master
+    git checkout $BRANCH
+    git rebase -i master
+    git commit --amend
+    git checkout master
+    git merge $BRANCH
+    git push origin master
+    git push origin :$BRANCH
+    git branch -D $BRANCH
+else
+    echo "not in a git repo..."

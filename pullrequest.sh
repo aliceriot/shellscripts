2a3a3770ef7ea06d6beb647202ccc042772b0caf
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-14 15:07:32 -0400

    hopefully handle force push better

--- pullrequest.sh
+++ pullrequest.sh
@@ -3,7 +3,7 @@
 if [ -d .git ]; then
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
     echo "creating pull request for "$BRANCH
-    if [ $1 == "-f" ]; then
+    if [ $1 == "f" ]; then
         git push origin $BRANCH -f
     else
         git push origin $BRANCH

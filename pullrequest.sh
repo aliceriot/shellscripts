c5ad4befb313b0d0e56371fb8c0411eee67727be
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-14 11:08:52 -0400

    open or update a PR on origin/featurebranch

---
+++ pullrequest.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -d .git ]; then
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    echo "creating pull request for "$BRANCH
+    if [ $1 == "-f" ]; then
+        git push origin $BRANCH -f
+    else
+        git push origin $BRANCH
+    fi
+else
+    echo "not in a git repo!"
+fi

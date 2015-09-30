b85ebc27ed0289a2bf260bba42492e09cf9e8ac9
Alice Pote <alice.writes.wrongs@gmail.com>
2015-09-30 10:18:01 -0400

    mergepr updates the remote branch, and conditionally deletes local and remote branches

--- mergepr.sh
+++ mergepr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # safely merge a feature branch and destroy it when you're done
+# there should already be an open pull request for $BRANCH on gh
 
 if [ -d .git ]; then
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -9,11 +10,16 @@ if [ -d .git ]; then
     git pull origin master
     git checkout $BRANCH
     git rebase -i master
+    git push origin $BRANCH -f
     git checkout master
     git merge $BRANCH
     git push origin master
-    git push origin :$BRANCH
-    git branch -D $BRANCH
+    if [[ $1 == 'd' ]]; then
+        git push origin :$BRANCH
+        git branch -D $BRANCH
+    else
+        echo "remote and local branches not deleted"
+    fi
 else
     echo "not in a git repo..."
 fi

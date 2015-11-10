3d105ef2c5c70b8a822ef09f782d4b9a0590aad9
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-10 09:15:59 -0500

    make the internal laptop screen shut off

--- i3externaldesk.sh
+++ i3externaldesk.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-xrandr --output HDMI1 --auto --above LVDS1 &&
-feh --bg-max ~/images/christhousemarthemarry.jpg
+xrandr --output HDMI1 --auto --above eDP1 &&
+xrandr --output eDP1 --off && 
+feh --bg-max ~/Pictures/parkspins.jpg

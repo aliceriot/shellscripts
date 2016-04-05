a1b188ec4ada66bed0f43ca9a553100b32427775
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-05 15:28:14 -0400

    new input names on my work thinkpad

--- i3externaldesk.sh
+++ i3externaldesk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-xrandr --output HDMI1 --auto --right-of eDP1 &&
+xrandr --output DP1 --auto --right-of eDP1 &&
 # xrandr --output eDP1 --off && 
 feh --bg-max ~/Pictures/parkspins.jpg

e108deb2fec172e999334ff9dff2b989cfe6a30e
Alice Pote <alice.writes.wrongs@gmail.com>
2016-06-28 11:13:49 -0400

    typo

--- i3externaldesk.sh
+++ i3externaldesk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-xrandr --output DP-2 --auto --right-of eDP1 &&
+xrandr --output DP2-2 --auto --right-of eDP1 &&
 xrandr --output eDP1 --off &&
 feh --bg-max ~/Pictures/tarsands.jpg
--- i3removedesk.sh
+++ i3removedesk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 xrandr --output eDP1 --auto &&
-xrandr --output DP-2 --off &&
+xrandr --output DP2-2 --off &&
 feh --bg-max ~/Pictures/tarsands.jpg

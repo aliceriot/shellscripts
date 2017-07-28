469778edfe3731c7a0c0c4fd84448e7c36910a7e
Alice Pote <alice.writes.wrongs@gmail.com>
2017-07-27 20:33:47 -0400

    redshift messes with it

--- nightmode.sh
+++ nightmode.sh
@@ -1,32 +1,10 @@
-#!/bin/csh
+#!/bin/bash
 # redscreen.csh Tue Jun 24 16:59:21 PDT 2014 Jeff Jahr
-
-# Usage
-if ($1 == "-h") then
-	echo "$0 [-h | dim | inv | off]"
-	exit
-endif
-
-# Return to normal settings
-if ( $1 == "off" ) then
-	xcalib -clear
-	exit
-endif
-
-if ($1 == "inv" ) then
-	xcalib -i -a
-	exit
-endif
-
-# Make the screen darker
-if ($1 == "dim" ) then
-	xcalib -clear
-	xcalib -co 30 -alter
-	exit
-endif
 
 # Turn the screen red
 # xcalib
+
+killall redshift
 xcalib -clear
 #xcalib -co 60 -alter
 xcalib -green .1 0 1 -alter
